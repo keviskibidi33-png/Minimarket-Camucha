104f50888b7b7283b172898db99adf811bd3fd38
Xangel0s <xangel0s@example.com>
2025-12-01 17:47:51 -0500

    fix: agregar googleClientId faltante a environments

--- minimarket-web/src/environments/environment.prod.ts
+++ minimarket-web/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.minimarketcamucha.com/api',
-  apiTimeout: 30000
+  apiTimeout: 30000,
+  googleClientId: '259590059487-5k8bk2sor6r9oa02pojkhj5nrd8c9h2e.apps.googleusercontent.com'
 };
 

01a77a662746cd19db2e713bf0589e205336a70f
Xangel0s <xangel0s@example.com>
2025-12-02 00:52:02 -0500

    feat: unificar frontend y backend con Nginx reverse proxy en puerto 3000

--- minimarket-web/src/environments/environment.prod.ts
+++ minimarket-web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.100.26:8080/api',
+  apiUrl: '/api',
   apiTimeout: 30000,
   googleClientId: '259590059487-5k8bk2sor6r9oa02pojkhj5nrd8c9h2e.apps.googleusercontent.com'
 };
